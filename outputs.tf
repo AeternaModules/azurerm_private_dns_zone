16bc493b076c0e7a225d3718ebebab82edee2b29
Daniel Allen <dan@aeternagroup.net>
2026-01-11 17:55:42 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "private_dns_zones" {
+  description = "All private_dns_zone resources"
+  value       = azurerm_private_dns_zone.private_dns_zones
+}
+
---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "private_dns_zones" {
+  description = <<EOT
+Map of private_dns_zones, attributes below
+Required:
+    - name
+    - resource_group_name
+Optional:
+    - tags
+    - soa_record (block):
+        - email (required)
+        - expire_time (optional)
+        - minimum_ttl (optional)
+        - refresh_time (optional)
+        - retry_time (optional)
+        - tags (optional)
+        - ttl (optional)
+EOT
+
+  type = map(object({
+    name                = string
+    resource_group_name = string
+    tags                = optional(map(string))
+    soa_record = optional(object({
+      email        = string
+      expire_time  = optional(number, 2419200)
+      minimum_ttl  = optional(number, 10)
+      refresh_time = optional(number, 3600)
+      retry_time   = optional(number, 300)
+      tags         = optional(map(string))
+      ttl          = optional(number, 3600)
+    }))
+  }))
+}
+
